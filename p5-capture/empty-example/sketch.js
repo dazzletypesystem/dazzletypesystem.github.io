14e290476dc7773255b5a34ac3df248f845eac4f
lorenzo bravi <ominoanonimo@gmail.com>
2018-04-09 15:16:08 +0200

    q

--- p5-capture/empty-example/sketch.js
+++ p5-capture/empty-example/sketch.js
@@ -3,7 +3,16 @@ function setup(){
 }
 
 function draw(){
-  image(video,0,0);
-  
+  background(255);
+  fingers.loadPixels();
+  var stepSize = round(constrain(mouseX / 8, 6, 32));
+  for (var y=0; y<height; y+=stepSize) {
+    for (var x=0; x<width; x+=stepSize) {
+      var i = y * width + x;
+      var darkness = (255 - fingers.pixels[i*4]) / 255;
+      var radius = stepSize * darkness;
+      ellipse(x, y, radius, radius);
+    }
+  }
   
 }
