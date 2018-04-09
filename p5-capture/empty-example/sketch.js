3d0eb9a98e892a0bfdbbbad117cd06237c3785ca
lorenzo bravi <ominoanonimo@gmail.com>
2018-04-09 15:27:53 +0200

    q

--- p5-capture/empty-example/sketch.js
+++ p5-capture/empty-example/sketch.js
@@ -4,12 +4,12 @@ function setup(){
 
 function draw(){
   background(255);
-  fingers.loadPixels();
+  video.loadPixels();
   var stepSize = round(constrain(mouseX / 8, 6, 32));
   for (var y=0; y<height; y+=stepSize) {
     for (var x=0; x<width; x+=stepSize) {
       var i = y * width + x;
-      var darkness = (255 - fingers.pixels[i*4]) / 255;
+      var darkness = (255 - video.pixels[i*4]) / 255;
       var radius = stepSize * darkness;
       ellipse(x, y, radius, radius);
     }
