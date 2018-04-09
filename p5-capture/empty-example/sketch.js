eec285eef9d461c71237dc1483968680a49374f5
lorenzo bravi <ominoanonimo@gmail.com>
2018-04-09 13:54:52 +0200

    q

--- p5-capture/empty-example/sketch.js
+++ p5-capture/empty-example/sketch.js
@@ -1,14 +1,50 @@
-var capture;
-
 function setup() {
-  createCanvas(390, 240);
-  capture = createCapture(VIDEO);
-  capture.size(320, 240);
-  //capture.hide();
+  
 }
 
 function draw() {
-  background(255);
-  image(capture, 0, 0, 320, 240);
-  filter('INVERT');
+  
 }
+
+navigator.getUserMedia  = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+
+var video = document.createElement('video');
+video.style.width =  document.width + 'px';
+video.style.height = document.height + 'px';
+video.setAttribute('autoplay', '');
+video.setAttribute('muted', '');
+video.setAttribute('playsinline', '');
+
+var facingMode = "environment";
+
+var constraints = {
+  audio: false,
+  video: {
+    facingMode: facingMode
+  }
+}
+
+navigator.mediaDevices.getUserMedia(constraints).then(function success(stream) {
+	video.srcObject = stream;
+});
+
+document.body.appendChild(video);
+
+video.addEventListener('click', function() {
+  if (facingMode == "user") {
+    facingMode = "environment";
+  } else {
+    facingMode = "user";
+  }
+  
+  constraints = {
+    audio: false,
+    video: {
+      facingMode: facingMode
+    }
+  }  
+  
+  navigator.mediaDevices.getUserMedia(constraints).then(function success(stream) {
+	  video.srcObject = stream;	
+  });
+});
