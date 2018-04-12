6ad0a28a23ea632abd0a4b88d47b094bc7017767
lorenzo bravi <ominoanonimo@gmail.com>
2018-04-12 16:32:06 +0200

    q

--- p5-capture/empty-example/sketch.js
+++ p5-capture/empty-example/sketch.js
@@ -32,9 +32,9 @@ function draw(v,c,bc,w,h) {
 	if(v.paused || v.ended)	return false;
 	// First, draw it into the backing canvas
 	bc.drawImage(v,0,0,w,h);
-	/*// Grab the pixel data from the backing canvas
+	// Grab the pixel data from the backing canvas
 	var idata = bc.getImageData(0,0,w,h);
-	var data = idata.data;
+	/*var data = idata.data;
 	// Loop through the pixels, turning them grayscale
 	for(var i = 0; i < data.length; i+=4) {
 		var r = data[i];
@@ -45,9 +45,9 @@ function draw(v,c,bc,w,h) {
 		data[i+1] = brightness;
 		data[i+2] = brightness;
 	}
-	idata.data = data;
+	idata.data = data;*/
 	// Draw the pixels onto the visible canvas
-	c.putImageData(idata,0,0);*/
+	c.putImageData(idata,0,0);
 	// Start over!
 	setTimeout(draw,20,v,c,bc,w,h);
 }
