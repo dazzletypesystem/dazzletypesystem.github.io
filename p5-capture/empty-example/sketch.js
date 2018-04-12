bb26ff51dae7cce5368e0854954c83f921e73e4a
lorenzo bravi <ominoanonimo@gmail.com>
2018-04-12 17:41:27 +0200

    q

--- p5-capture/empty-example/sketch.js
+++ p5-capture/empty-example/sketch.js
@@ -65,7 +65,7 @@ function drawF(v,c,bc,w,h) {
 	// Draw the pixels onto the visible canvas
 	c.putImageData(idata,0,0);
 	
-	copy(0, 0, width, height, 0, 0, width, height);
+	copy(0, 0, width, height, 0, 0, 100, 100);
 	image(img,0,0,200,200);
 	// Start over!
 	setTimeout(drawF,20,v,c,bc,w,h);
